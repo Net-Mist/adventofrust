909e3bd9cc8f1a297b10fa49abb9c2e7b21559ce
Sebastien Iooss <archimist.linux@gmail.com>
2022-12-21 09:41:58 +0100

    part2

--- 2022/day_21/src/main.rs
+++ 2022/day_21/src/main.rs
@@ -8,116 +8,6 @@ use nom::number::complete::float;
 use nom::{bytes::complete::tag, sequence::tuple, IResult};
 use std::collections::HashMap;
 
-
-
-#[derive(Debug, Clone, Copy)]
-enum Operation {
-    Sum((&'static str, &'static str)),
-    Sub((&'static str, &'static str)),
-    Mult((&'static str, &'static str)),
-    Div((&'static str, &'static str)),
-    Value(i128),
-}
-
-fn num_parser(l: &str) -> IResult<&str, u32> {
-    map(float, |x| x as u32)(l)
-}
-
-fn parser(b: &'static str) -> IResult<&str, (&str, Operation)> {
-    alt((
-        map(
-            tuple((
-                take_until1(":"), //6
-                tag(": "),
-                num_parser,
-            )),
-            |r| (r.0, Operation::Value(r.2 as i128)),
-        ),
-        map(
-            tuple((
-                take_until1(":"),
-                tag(": "),
-                take_until1(" "),
-                tag(" "),
-                take_until1(" "), //4
-                tag(" "),
-                take(4usize),
-            )),
-            |r| {
-                let operation = match r.4 {
-                    "+" => Operation::Sum((r.2, r.6)),
-                    "-" => Operation::Sub((r.2, r.6)),
-                    "*" => Operation::Mult((r.2, r.6)),
-                    "/" => Operation::Div((r.2, r.6)),
-                    _ => unreachable!(),
-                };
-                (r.0, operation)
-            },
-        ),
-    ))(b)
-}
-
-fn data() -> HashMap<&'static str, Operation> {
-    include_str!("../i.txt")
-        .lines()
-        .map(|b| parser(b).unwrap().1)
-        .collect()
-}
-
-
-fn part1() {
-    let mut monkeys = data();
-    println!("{:?}", monkeys);
-
-    let mut values = HashMap::new();
-
-    while !values.contains_key(&"root") {
-        let mut val_to_remove = Vec::new();
-        for (&k, v) in monkeys.iter() {
-            if let Operation::Value(value) = v {
-                values.insert(k, *value);
-                val_to_remove.push(k);
-            }
-            match v {
-                Operation::Sum((a, b)) => {
-                    if values.contains_key(a) && values.contains_key(b) {
-                        let val = *values.get(a).unwrap() + *values.get(b).unwrap();
-                        values.insert(k, val);
-                        val_to_remove.push(k);
-                    }
-                }
-                Operation::Sub((a, b)) => {
-                    if values.contains_key(a) && values.contains_key(b) {
-                        let val = *values.get(a).unwrap() - *values.get(b).unwrap();
-                        values.insert(k, val);
-                        val_to_remove.push(k);
-                    }
-                }
-                Operation::Mult((a, b)) => {
-                    if values.contains_key(a) && values.contains_key(b) {
-                        let val = *values.get(a).unwrap() * *values.get(b).unwrap();
-                        values.insert(k, val);
-                        val_to_remove.push(k);
-                    }
-                }
-                Operation::Div((a, b)) => {
-                    if values.contains_key(a) && values.contains_key(b) {
-                        let val = *values.get(a).unwrap() / *values.get(b).unwrap();
-                        values.insert(k, val);
-                        val_to_remove.push(k);
-                    }
-                }
-                _ => {}
-            }
-        }
-        for v in val_to_remove.into_iter() {
-            monkeys.remove(v);
-        }
-    }
-    println!("{:?}", values.get(&"root"));
-}
-
-// for part 2 implement full tree
 #[derive(Debug, Clone, Copy)]
 struct Op {
     op: &'static str,
@@ -132,29 +22,214 @@ enum Node {
     Op(Op)
 }
 
-fn do_op(op: &Operation, v1: i128, v2: i128) -> i128{
+
+
+fn num_parser(l: &str) -> IResult<&str, u32> {
+    map(float, |x| x as u32)(l)
+}
+
+fn parser(b: &'static str) -> IResult<&str, (&str, Node)> {
+    alt((
+        map(
+            tuple((
+                take_until1(":"), //6
+                tag(": "),
+                num_parser,
+            )),
+            |r| (r.0, Node::Value(r.2 as i128)),
+        ),
+        map(
+            tuple((
+                take_until1(":"),
+                tag(": "),
+                take_until1(" "),
+                tag(" "),
+                take_until1(" "), //4
+                tag(" "),
+                take(4usize),
+            )),
+            |r| {
+                (r.0, Node::Op( Op {
+                    op: r.4,
+                    node_i_l: r.2,
+                    node_i_r: r.6
+                }))
+            },
+        ),
+    ))(b)
+}
+
+fn data() -> HashMap<&'static str, Node> {
+    include_str!("../i.txt")
+        .lines()
+        .map(|b| parser(b).unwrap().1)
+        .collect()
+}
+
+fn do_op(op: &str, a:i128, b:i128) -> i128{
     match op {
-        Operation::Sum(_) => {v1+v2},
-        Operation::Sub(_) => {v1-v2},
-        Operation::Mult(_) => {v1*v2},
-        Operation::Div(_) => {v1/v2},
+        "+" => {a + b},
+        "-" => {a - b},
+        "*" => {a * b},
+        "/" => {a / b},
         _ => unreachable!()
     }
 }
 
+fn do_reverse_op_l(op: &str, a:i128, r:i128) -> i128{
+    match op {
+        "+" => {r - a},
+        "-" => {a - r},
+        "*" => {r / a},
+        "/" => {a / r},
+        _ => unreachable!()
+    }
+}
+
+fn do_reverse_op_r(op: &str, a:i128, r:i128) -> i128{
+    match op {
+        "+" => {r - a},
+        "-" => {a + r},
+        "*" => {r / a},
+        "/" => {a * r},
+        _ => unreachable!()
+    }
+}
+
+fn part1() {
+    let mut monkeys = data();
+    loop {
+        let mut value_to_insert = Vec::new();
+        let mut value_to_remove = Vec::new();
+
+        for &k in monkeys.keys() {
+            let node =  monkeys.get(k).unwrap();
+            match node {
+                Node::Value(_) | Node::Unknown => continue,
+                Node::Op(op) => {
+                    match (monkeys.get(op.node_i_l).unwrap(), monkeys.get(op.node_i_r).unwrap()) {
+                        (Node::Value(a), Node::Value(b)) => {
+                            let v = do_op(op.op, *a, *b);
+                            value_to_insert.push((k, Node::Value(v)));
+                            value_to_remove.push(op.node_i_l);
+                            value_to_remove.push(op.node_i_r);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        if value_to_insert.is_empty() {
+            break;
+        }
+        for (k, v) in value_to_insert {
+            monkeys.insert(k, v);
+        }
+        for k in value_to_remove {
+            monkeys.remove(k);
+        }
+    }
+    println!("{:?}", monkeys.get("root").unwrap());
+}
+
 fn part2() {
     let mut monkeys = data();
-    println!("{:?}", monkeys);
-    let mut values = HashMap::new();
 
-    // fill values
-    for (n, m) in monkeys.into_iter() {
+    // correct mistakes
+    let root_op = monkeys.get(&"root").unwrap();
+    let mut root_left = "";
+    let mut root_right = "";
+    let new_root_op = match root_op {
+        Node::Op(op) => {
+            root_left = op.node_i_l;
+            root_right = op.node_i_r;
+            Op {
+                op: &"=",
+                node_i_l: op.node_i_l,
+                node_i_r: op.node_i_r
+            }
+        }
+        _ => unreachable!()
+    };
+    monkeys.insert(&"root", Node::Op(new_root_op));
+    monkeys.insert(&"humn", Node::Unknown);
+
+    // simplify tree
+    loop {
+        let mut value_to_insert = Vec::new();
+        let mut value_to_remove = Vec::new();
+
+        for &k in monkeys.keys() {
+            let node =  monkeys.get(k).unwrap();
+            match node {
+                Node::Value(_) | Node::Unknown=> continue,
+                Node::Op(op) => {
+                    match (monkeys.get(op.node_i_l).unwrap(), monkeys.get(op.node_i_r).unwrap()) {
+                        (Node::Value(a), Node::Value(b)) => {
+                            let v = do_op(op.op, *a, *b);
+                            value_to_insert.push((k, Node::Value(v)));
+                            value_to_remove.push(op.node_i_l);
+                            value_to_remove.push(op.node_i_r);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        if value_to_insert.is_empty() {
+            break;
+        }
+
+        for (k, v) in value_to_insert {
+            monkeys.insert(k, v);
+        }
+        for k in value_to_remove {
+            monkeys.remove(k);
+        }
+    }
+
+    // solve
+    let mut node_to_solve;
+    let mut value;
+    match (monkeys.get(root_left).unwrap(), monkeys.get(root_right).unwrap()) {
+        (Node::Value(v), _ ) => {value = *v; node_to_solve=root_right},
+        (_, Node::Value(v))  => {value = *v; node_to_solve=root_left},
+        _ => unreachable!()
+    }
+
+    while node_to_solve != "humn" {
+        let node = monkeys.get(node_to_solve).unwrap();
+        match node {
+            Node::Op(op) => {
+                match (monkeys.get(op.node_i_l).unwrap(), monkeys.get(op.node_i_r).unwrap()) {
+                    (Node::Value(a), _) => {
+                        value = do_reverse_op_l(op.op, *a, value);
+                        node_to_solve = op.node_i_r
+                    }
+                    (_, Node::Value(a)) => {
+                        value = do_reverse_op_r(op.op, *a, value);
+                        node_to_solve = op.node_i_l
+                    }
+                    _ => {
+                        panic!()
+                    }
+                    
+                }
+            },
+            _ => unreachable!()
+        }
 
     }
+    println!("{:?}", value);
+
+
+    
 }
 
 
 fn main() {
-    // part1();
+    part1();
     part2();
 }
